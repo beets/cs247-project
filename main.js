5aa4ea95c59830115bc4f0fcdba4f90842388e70
beets <rrotness@Carolyns-iMac.local>
2015-03-07 17:50:09 -0800

    Convert existing app to templates

--- main.js
+++ main.js
@@ -1,22 +1,34 @@
 Router.route('/timeline', function() {
     this.layout('layout');
     this.render('timeline');
+
+    Template.timeline_new.events({
+        "click .timeline_new .add_story": function() {
+            Router.go('/story/add');
+        },
+    });
 });
 
 Router.route('/story/add', function() {
     this.layout('layout');
-    this.render('story-add');
-
-});
+    this.render('story_add');
 
     if (Meteor.isClient) {
-        Template.body.events({
+        Template.story_add.events({
             "click .upload-photo": function() {
                 console.log('hello');
             },
-
             "click #send": function() {
                 Router.go('/timeline');
-            },
+            }
         });
+
+/*
+        Template.story_add.helpers({
+            comments: [
+                {
+            ]
+        });
+*/
     }
+});
