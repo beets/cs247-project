2f75cab8845e41d8cbfb3b648fdac15b18538723
Shailesh Tainwala <shailesh.tainwala@gmail.com>
2015-03-08 20:07:44 -0700

    Story now correctly sends data to story_response, which is then added to the json file

--- save_response.php
+++ save_response.php
@@ -5,7 +5,7 @@ $family_json = json_decode($family_file, true);
 $storyId = $_GET['id'];
 
 //The page that calls this URL will send values for responseText (the story that grandma typed out)
-$family_json['stories'][$storyId]["responseText"] = "NEW REPONSE TEXT";
+$family_json['stories'][$storyId]["responseText"] = $_POST['responseText'];
 $output = json_encode($family_json, JSON_PRETTY_PRINT);
 file_put_contents('./data/' . $family . '.json', $output);
 //
