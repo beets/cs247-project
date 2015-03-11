d809283a22645cc57b6227e92e7a31844239e17a
beets <beetness@gmail.com>
2015-03-10 17:56:58 -0700

    add prompt to link

--- add_story_post.php
+++ add_story_post.php
@@ -25,20 +25,26 @@ $link = 'http://'.$_SERVER['SERVER_NAME'].'/moments/story.php?'.$_SERVER['QUERY_
     <h2>Hooray, you've created a story!</h2>
     <div class="row main-photo-row">
         <div class="col-xs-12">
-            <div class="main-photo" style="background-image:url(<?= $_POST['photo_url']?>);"></div>
+            <img class="img main-photo" src="<?= $_POST['photo_url']?>" />
         </div>
     </div>
-    <h3>Send this link to your parent to get the story</h3>
+    <h3>Send this message to your parent to get the story</h3>
     <div class="row">
         <div class="col-xs-12 form-group form-group-lg">
-            <input type="text" name="link" class="form-control" value="<?= $link ?>" />
+            <textarea name="link" class="form-control">
+<?= $newStory['prompt'] ?>
+
+
+Moments is an application that helps record family memories. Visit this page to record yours:
+<?= $link ?>
+            </textarea>
         </div>
     </div>
     <a class="btn btn-primary" href="timeline.php?<?= $_SERVER['QUERY_STRING']?>">Back to timeline</a>
 </div>
 <? include 'templates/footer.html' ?>
 <script>
-    $('input[name=link]').click(function() {
+    $('textarea[name=link]').click(function() {
         $(this).select();
     });
 </script>
