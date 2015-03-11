9f8a01fe02f861840d6a98c5c59930a5b160a6fb
beets <rrotness@Carolyns-iMac.local>
2015-03-11 02:17:32 -0700

    Action indicator
    are the circles the right place for it?

--- timeline.php
+++ timeline.php
@@ -20,10 +20,18 @@ function new_story() {
 
 function existing_memory($id, $memory) {
   global $get, $user_id;
+  $responses = $memory['responses'];
   $completed = false;
-  for ($i = 0; $i < count($memory['responses']); ++$i) {
-    if ($memory['responses'][$i]['member'] == $user_id) {
+  $video_count = 0;
+  $text_count = 0;
+  for ($i = 0; $i < count($responses); ++$i) {
+    if ($responses[$i]['member'] == $user_id) {
       $completed = true;
+      if ($responses[$i]['video_url']) {
+        $video_count++;
+      } else {
+        $text_count++;
+      }
     }
   }
   $url = 'memory.php?' . $get . '&id=' . $id;
@@ -34,15 +42,24 @@ function existing_memory($id, $memory) {
       $url .= '&edit=1';
   }
   ?>
-  <li class="existing-memory <?=$class?>">
+  <li class="existing-memory timeline-inverted <?=$class?>">
     <a href="<?= $url ?>">
-        <div class="tl-circ"></div>
+        <div class="tl-circ">
+            <? if (!$completed) { ?>
+            <span class="glyphicon glyphicon-pencil"></span>
+            <? } else { ?>
+            <span class="glyphicon glyphicon-ok"></span>
+            <? } ?>
+        </div>
         <div class="timeline-panel">
             <div class="tl-body">
               <div class="images row">
                   <img src="<?= $memory['photo_url'] ?>" class="img img-responsive center"/>
               </div>
               <h4 class="title"><?= $memory['title'] ?></h4>
+              <div class="text-right">
+                <span class="glyphicon glyphicon-facetime-video"></span> <?= $video_count ?>&nbsp;&nbsp; 
+                <span class="glyphicon glyphicon-envelope"></span> <?= $text_count ?></p>
             </div>
         </div>
     </a>
@@ -54,10 +71,7 @@ function existing_memory($id, $memory) {
 <? include 'templates/header.html' ?>
 <? include 'templates/nav.html' ?>
 <div id="main-timeline" class="container-fluid">
-    <h1><?= $parent ?>'s lifetime of experiences</h1>
-    <div class="row header">
-        <img class="grandpa-main" src="./data/<?= $family ?>/parent.jpg" />
-    </div>
+    <h1><?= $family_name ?> memories</h1>
     <div class="timeline-container">
         <ul class="timeline">
             <? for ($i = count($memories) - 1; $i >= 0; --$i) { ?>
